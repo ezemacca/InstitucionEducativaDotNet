9a2d36ed4e7e0a0a71e1f2cbd913b97b3c50156c
Nicocerve <44000640+Nicocerve@users.noreply.github.com>
2022-06-04 17:25:50 -0300

    Agregacion minima

--- AL/AL.Aplicacion/interfaces/IRepositorioCurso.cs
+++ AL/AL.Aplicacion/interfaces/IRepositorioCurso.cs
@@ -1,1 +1,12 @@
+using AL.Aplicacion.Entidades;
 namespace AL.Aplicacion.Interfaces;
+
+public interface IRepositorioCurso
+{
+    Curso? GetCurso();
+    List<Curso> GetCursos();
+    void ModificarCurso(Curso curso);
+    void EliminarCurso(Curso curso);
+    void AgregarCurso(Curso curso);
+
+}
