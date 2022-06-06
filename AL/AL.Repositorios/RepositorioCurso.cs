9f0f362865bc5aac3686fe98673e7c767a9058bf
Nicocerve <44000640+Nicocerve@users.noreply.github.com>
2022-06-06 14:48:33 -0300

    Esqueleto

---
+++ AL/AL.Repositorios/RepositorioCurso.cs
@@ -0,0 +1,15 @@
+using AL.Aplicacion.Entidades;
+using AL.Aplicacion.Interfaces;
+namespace AL.Repositorios;
+public class RepositorioCurso : IRepositorioCurso
+{
+
+    public void AgregarCurso(Curso curso) 
+    {
+
+    }
+    public void EliminarCurso(int id) 
+    {
+
+    }
+}
