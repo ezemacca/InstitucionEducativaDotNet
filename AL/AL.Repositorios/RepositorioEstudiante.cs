7297f5a3529847ceaafeacf4d8dacc649ae99b3f
Ezequiel Miguel Maccarini <43655619+ezemacca@users.noreply.github.com>
2022-06-19 20:38:03 -0300

    Anda todo 🥳

--- AL/AL.Repositorios/RepositorioEstudiante.cs
+++ AL/AL.Repositorios/RepositorioEstudiante.cs
@@ -95,16 +95,38 @@ public class RepositorioEstudiante : IRepositorioEstudiante
 
     public List<Estudiante> GetEstudiantesConUnCursoFinalizado()
     {
-        using(var context = new InstitucionEducativaContext())
+       using(var context = new InstitucionEducativaContext())
         {
-            List<Estudiante> estudiantesAntiguos= new List<Estudiante>();
-            List<Curso> cursosTerminados= context.Cursos.Include(c => c.Inscripciones)
-                .ThenInclude(i => i.Estudiante)
-                .Where(cur => cur.FechaFin  < DateTime.Now)
-                .ToList();
-            cursosTerminados.ForEach(c => c.Inscripciones.ForEach(i => estudiantesAntiguos.Add(new Estudiante(){ Nombre=i.Estudiante.Nombre, Apellido=i.Estudiante.Apellido, Inscripciones= new List<Inscripcion>(){i}})));
-            return estudiantesAntiguos;           
+            
+            List<Estudiante> estudiantes= context.Estudiantes.Include(est => est.Inscripciones).ThenInclude(ins=> ins.Curso).ToList();
+            List<Estudiante> e_curso_finalizado= new List<Estudiante>();
+            foreach (Estudiante e in estudiantes){
+                if(e.Inscripciones!=null){
+                    foreach(Inscripcion i in e.Inscripciones){
+                        // si el curso esta activo
+                        if((i.Curso!= null) && (i.Curso.FechaFin<DateTime.Now)){
+                            // Si el estudiante no estaba en la lista 
+                            if(e_curso_finalizado.Find(est=> est.DNI==e.DNI)==null){
+                                e_curso_finalizado.Add(
+                                    new Estudiante()
+                                    {   
+                                        Nombre=e.Nombre, 
+                                        Apellido=e.Apellido,
+                                        DNI=e.DNI, 
+                                        Inscripciones= new List<Inscripcion>(){i}
+                                    }
+                                );
+                            }
+                            else
+                            {
+                                e_curso_finalizado.Find(est=> est.DNI==e.DNI).Inscripciones.Add(i);
+                            }
+                        }
+                    }
+                }
+            }
+            context.SaveChanges();
+            return e_curso_finalizado;
         }
-        return null;
-    }
+    }   
 }
